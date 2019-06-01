00d58fe7ba9d7c4a3fc8c2a9b64f2c283ca05084
WentTheFox <mail@went.tf>
2019-06-01 06:26:55 -0700

    Add OpenAPI type generation

---
+++ src/app/types/api-aliases.ts
@@ -0,0 +1,10 @@
+export * from './api';
+import * as api from './api';
+
+/**
+ * @file
+ * Used to create aliases for lengthy generated OpenAPI operation IDs
+ */
+
+export type GetAllAppearancesRequest = api.AppControllersApiAppearancesControllerGetAllRequest;
+export type GetAllAppearancesResult = api.AppControllersApiAppearancesControllerGetAllResult;
