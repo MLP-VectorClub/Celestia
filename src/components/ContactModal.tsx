c2995f7262493b91645d9b229b8ba09cfa187361
WentTheFox <mail@went.tf>
2020-06-13 05:14:57 +0200

    Move contact modal text into translations

--- src/components/ContactModal.tsx
+++ src/components/ContactModal.tsx
@@ -1,34 +1,47 @@
 import { Modal, ModalBody, ModalHeader } from 'reactstrap';
 import { useDispatch, useSelector } from 'react-redux';
-import { DEV_EMAIL, DISCORD_INVITE_LINK } from '../config';
+import { CLUB_URL, DEV_EMAIL, DISCORD_INVITE_LINK } from '../config';
 import { RootState } from '../store/rootReducer';
 import { coreActions } from '../store/slices';
 import ExternalLink from './shared/ExternalLink';
+import { useTranslation, Trans } from '../i18n';
 
 export default (() => {
+  const { t } = useTranslation('common');
   const dispatch = useDispatch();
   const { contactOpen } = useSelector((state: RootState) => state.core);
   const toggle = () => dispatch(coreActions.toggleContact());
   return (
     <Modal isOpen={contactOpen} toggle={toggle}>
-      <ModalHeader toggle={toggle}>Contact Us</ModalHeader>
+      <ModalHeader toggle={toggle}>{t('contact.header')}</ModalHeader>
       <ModalBody>
-        <h3>How to contact us</h3>
-        <p>You can use any of the following methods to reach out to us:</p>
+        <h3>{t('contact.howTo')}</h3>
+        <p>{t('contact.useAnyBelow')}</p>
         <ul>
           <li>
-            <ExternalLink href={DISCORD_INVITE_LINK}>
-              Join our Discord server
-            </ExternalLink>
-            and describe your issue/idea in the <strong>#support</strong> channel
+            <Trans ns="common" i18nKey="contact.discord">
+              <ExternalLink href={DISCORD_INVITE_LINK}>
+                0
+              </ExternalLink>
+              1
+              <strong>2</strong>
+              3
+            </Trans>
           </li>
           <li>
-            <ExternalLink href="https://www.deviantart.com/mlp-vectorclub/notes/">
-              Send a note
-            </ExternalLink>
-            to the group on DeviantArt
+            <Trans ns="common" i18nKey="contact.deviantart">
+              <ExternalLink href={`${CLUB_URL}/notes/`}>
+                0
+              </ExternalLink>
+              1
+            </Trans>
           </li>
-          <li><a href={`mailto:${DEV_EMAIL}`}>Send an e-mail</a> to {DEV_EMAIL}</li>
+          <li>
+            <Trans ns="common" i18nKey="contact.email" values={{ email: DEV_EMAIL }}>
+              <a href={`mailto:${DEV_EMAIL}`}>0</a>
+              1
+            </Trans>
+          </li>
         </ul>
       </ModalBody>
     </Modal>
