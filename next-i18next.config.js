82e47e7b09349633ca04e1c48768e654b92e6c24
WentTheFox <mail@went.tf>
2021-06-20 14:06:23 +0200

    Attempt to fix locale error causing app-wide crash during SSR

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,9 @@
+const path = require('path');
+
 module.exports = {
   i18n: {
     locales: ['en'],
     defaultLocale: 'en',
   },
+  localePath: path.resolve(path.join(__dirname, 'public', 'locales')),
 };
