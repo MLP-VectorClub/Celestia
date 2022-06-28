d377d8607fe3b46bd49e2989b033dfb1d90dd117
WentTheFox <mail@went.tf>
2022-06-28 18:48:55 +0200

    Upgrade to Next.js 12.2

--- apps/celestia/src/components/logos/FriendshipIsMagicLogo.tsx
+++ apps/celestia/src/components/logos/FriendshipIsMagicLogo.tsx
@@ -1,8 +1,7 @@
 import Image, { ImageProps } from 'next/image';
 import { VFC } from 'react';
 import { getGuideLabel } from 'src/utils';
-import fimLogo from '../../../public/img/logos/pony.svg';
 
 export const FriendshipIsMagicLogo: VFC<Pick<ImageProps, 'priority'>> = ({ priority }) => (
-  <Image src={fimLogo as StaticImageData} width={1006.477} height={1154.755} alt={`${getGuideLabel('pony')} Logo`} priority={priority} />
+  <Image src="/img/logos/pony.svg" width={1006.477} height={1154.755} alt={`${getGuideLabel('pony')} Logo`} priority={priority} />
 );
