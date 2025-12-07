e3de6e01a3ae3da526aeb305f9d415ae7f00a125
WentTheFox <mail@went.tf>
2025-12-07 10:03:57 +0100

    Fix pagination on show page

--- apps/celestia/src/hooks/show.ts
+++ apps/celestia/src/hooks/show.ts
@@ -7,7 +7,7 @@ import { useMemo } from 'react';
 export const useShowList = (params: GetShowRequest, initialData?: GetShowResult) => {
   const key = ENDPOINTS.SHOW(params);
   const fetcher = useMemo(() => showListFetcher(params), [params]);
-  const { data, error: showError } = useQuery(key, fetcher, { initialData });
+  const { data, error: showError } = useQuery(key, fetcher, { initialData, keepPreviousData: false });
 
   return {
     data,
